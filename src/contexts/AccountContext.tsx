cb2893443c7d279647af68992225e98bbdbfefc5
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-10-31 09:54:09 +0700

    ➕ Add AccountLogin context

---
+++ src/contexts/AccountContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, ReactNode } from 'react';
+
+interface AccountProviderProps {
+  children: ReactNode;
+}
+
+interface AccountContextValue {
+  isAuth: boolean;
+  username: string;
+  name: string;
+  avt: string;
+}
+
+const defaultValue: AccountContextValue = {
+  isAuth: false,
+  username: '',
+  name: '',
+  avt: '',
+};
+
+export const AccountContext = createContext<AccountContextValue>(defaultValue);
+
+const AccountContextProvider = ({ children }: AccountProviderProps) => {
+  return (
+    <AccountContext.Provider value={defaultValue}>
+      {children}
+    </AccountContext.Provider>
+  );
+};
+
+export default AccountContextProvider;
